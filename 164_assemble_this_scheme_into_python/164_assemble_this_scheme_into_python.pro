54d95551b00bc34b1764a86a79c8f51a11d9796a
oopmonkey <dddworld@gmail.com>
2014-05-29 19:25:16 -0400

    added challenge 164 of already existing process

---
+++ 164_assemble_this_scheme_into_python/164_assemble_this_scheme_into_python.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-05-27T11:26:00
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = 164_assemble_this_scheme_into_python
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ 164_assemble_this_scheme_into_python/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <QTextStream>
+#include <array>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QTextStream qin(stdin);
+    QTextStream qout(stdout);
+
+    // Output 'Hello World' to the console.
+    qout << "Hello World!" << endl;
+
+    // Return an array of the first 100 numbers that are divisible by 3 and 5.
+    for(int i = 0; i <= 100; ++i)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+            qout << i << endl;
+    }
+
+    // Create a program that verifies if a word is an anagram of another word.
+
+    // Create a program that removes a specificed letter from a word.
+    qout << "Please input a word:" << endl;
+    QString targetWord;
+    qin >> targetWord;
+
+    qout << endl << "Input a letter to be removed the word you just entered: " << endl;
+    QString targetLetter;
+    qin >> targetLetter;
+    qout << targetWord.replace(targetLetter, "") << endl;
+
+    // Sum all the elements of an array
+    array<int, 100> numbers;
+    int sum = 0;
+    numbers.fill(10);
+    for(unsigned int i = 0; i < numbers.size(); ++i)
+    {
+        sum += numbers[i];
+    }
+    qout << "Sum of " << numbers.size() << " numbers: " << sum << endl;
+
+    // BONUS: Implement a bubble-sort.
+
+    return 0;
+}
+
+bool isAnagram(QString word1, QString word2) {
+    if(word1.size() != word2.size())
+        return false;
+
+}
